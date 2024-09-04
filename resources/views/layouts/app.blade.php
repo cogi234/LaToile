c62838ec74dd75f24662460d9bd2942befc29005
cogi234 <colinbougie@gmail.com>
2024-09-04 10:44:14 -0400

    Dashboard layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,12 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            <livewire:layout.navigation />
+            @auth
+                <livewire:layout.navigation />
+            @endauth
+            @guest
+                <livewire:layout.guest />
+            @endguest
 
             <!-- Page Heading -->
             @if (isset($header))
