d4a913f52ddc928bd25405ad078aa264189a5fd9
cogi234 <colinbougie@gmail.com>
2024-09-06 20:14:14 -0400

    Simplifier
    On veux juste creer des posts de texts.
    Pas besoin de gosser pour pouvoir en modifier ou gerer des images pour l'instant

--- resources/views/livewire/posts/create.blade.php
+++ resources/views/livewire/posts/create.blade.php
@@ -2,19 +2,14 @@
 
 use Livewire\Volt\Component;
 use Livewire\Attributes\Validate;
-use Livewire\Attributes\Locked;
 use App\Models\Post;
 
 new class extends Component {
-    #[Locked]
-    public int $id;
+    #[Validate('required|string|min:3')]
+    public string $text = "";
 
-    #[Validate('required')]
-    public array $post_content;
-
-    private function splitParagraphs(string $block_content) : string {
+    private function splitParagraphs(string $block_content) : array {
         $blocks = [];
-
         $paragraphs = explode("\n", $block_content);
 
         foreach ($paragraphs as $paragraph) {
@@ -25,68 +20,36 @@ new class extends Component {
                 ];
             }
         }
+
+        return $blocks;
     }
     
     public function store() {
-        $encoded_content = json_encode(array_filter($post_content, function($block) {
+        $validated = $this->validate();
+        
+        $blocks = $this->splitParagraphs($validated['text']);
+        $encoded_content = json_encode(array_filter($blocks, function($block) {
             return strlen(trim($block['content'])) > 0; //We filter out empty blocks
         }));
+        
+        $post = new Post;
+        $post->content = $encoded_content;
+        $post->user_id = Auth::user()->id;
+        $post->save();
 
-        if ($this->id < 0) {
-            //This is a new post
-            $post = new Post;
-            $post->content = $encoded_content;
-            $post->user_id = Auth::user()->id;
-            $post->save();
-        } else {
-            //We are editing a post
-            $post = Post::find($this->id);
-            $post->content = $encoded_content;
-            $post->save();
-        }
-        $this->reset();
-    }
-
-    public function mount($post = null) {
-        $this->id = -1;
-        $this->post_content = [ [ 'type' => 'text', 'content' => '' ] ];
-        if ($post != null) {
-            $this->id = $post->id;
-            $this->post_content = json_decode($post->content, true);
-        }
+        $this->text = "";
     }
 
 }; ?>
 
 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 my-4">
     <form wire:submit='store'>
-        <div
-            class="block w-full border-2 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-            style="border:1px"
-        >
-            @foreach ($post_content as $block)
-                @switch($block['type'])
-                    @case('text')
-                        <textarea 
-                            class="block w-full bg-white dark:bg-gray-800" 
-                            style="border:0px"
-                            wire:model='content.{{$loop->index}}' 
-                        @if ($loop->first)
-                            placeholder='Partagez vos pensées'
-                        @endif
-                        ></textarea>
-                        @break
-                    @case('image')
-                        @break
-                    @case('video')
-                        @break
-                    @case('audio')
-                        @break
-                    @case('poll')
-                        @break
-                @endswitch
-            @endforeach
-        </div>
+        <textarea
+            wire:model="text"
+            placeholder="Partagez vos pensées"
+            class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+        ></textarea>
+        <div class="text-red-600 font-bold mt-2">@error('text') {{ $message }} @enderror</div>
         <x-primary-button class="mt-4">{{ __('Publier') }}</x-primary-button>
     </form>
 </div>
