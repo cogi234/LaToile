7fe1dcdaa25df193d73b37f465c18dd8df866d6a
cogi234 <colinbougie@gmail.com>
2024-09-06 21:27:31 -0400

    colin account et pas de form si pas connecter

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <livewire:posts.create />
+            @auth
+                <livewire:posts.create />
+            @endauth
             <br />
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
