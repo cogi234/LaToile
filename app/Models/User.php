25e900a2712f0dc8547927674fd7e18486ee0b7b
VincentLajoie <125910689+THzWeeds@users.noreply.github.com>
2024-09-09 17:22:56 -0400

    email/mailtrap
    manque le message custum a faire demain

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,15 +1,15 @@
 <?php
 
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+ 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
@@ -113,4 +113,5 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Tag::class, "blocked_tags");
     }
+
 }
