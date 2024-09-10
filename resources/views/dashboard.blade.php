c3a4cbc61886f9d1357ece7c8ee669ddb5f9da4a
NAGZYY <68570686+NAGZYY@users.noreply.github.com>
2024-09-10 06:21:09 -0400

    Post visible pour tout le monde
    2024-09-10

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,9 +26,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div id="suivis-content" class="content-section" style="display: block;">
-                        @auth
-                            <livewire:posts.post />
-                        @endauth
+                        <livewire:posts.post />
                     </div>
                     <div id="abonnements-content" class="content-section" style="display: none;">
                         <h2>{{ __('Posts Suivis') }}</h2>
