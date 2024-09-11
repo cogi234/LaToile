6a35f3050bdd20667d3bc32e36a86f60d9d9c48a
cogi234 <colinbougie@gmail.com>
2024-09-11 10:37:22 -0400

    scroll la liste

--- resources/views/livewire/posts/viewall.blade.php
+++ resources/views/livewire/posts/viewall.blade.php
@@ -5,30 +5,23 @@ use App\Models\Post;
 
 new class extends Component {
     public $posts = [];
-    public $page = 1;
-    public $hasMorePages = true;
+    public $moreAvailable = true;
 
     public function mount()
     {
-        $this->loadPosts();
-    }
-
-    public function loadPosts()
-    {
-        $paginator = Post::orderby("created_at")->paginate(10, ['*'], 'page', $this->page);
-
-        // Merge the new posts with the existing ones
-        $this->posts = array_merge($this->posts, $paginator->items());
-
-        // Check if there are more pages to load
-        $this->hasMorePages = $paginator->hasMorePages();
+            $this->posts = Post::orderby('id', 'desc')->take(10)->get();
     }
 
     public function loadMore()
     {
-        if ($this->hasMorePages) {
-            $this->page++;
-            $this->loadPosts();
+        if ($this->moreAvailable) {
+            $newPosts = Post::where('id', '<', $this->posts->last()->id)->orderby('id', 'desc')->take(10)->get();
+
+            // Merge the new posts with the existing ones
+            $this->posts = $this->posts->merge($newPosts);
+
+            // Check if there are more pages to load
+            $this->moreAvailable = $newPosts->isNotEmpty();
         }
     }
 }; ?>
@@ -39,8 +32,8 @@ new class extends Component {
         <x-post-view :post="$post">{{ $post->title }}</x-post-view>
     @endforeach
 
-    @if ($hasMorePages)
-        <div wire:loading>Chargement de plus de posts...</div>
+    @if ($moreAvailable)
+        <div wire:click='loadMore'>Chargement de plus de posts...</div>
     @else
         <div>Il n'y a plus de post à voir.</div>
     @endif
