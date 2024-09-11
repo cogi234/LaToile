1592cc480b82adda434d1e5f61298c4232112e8a
VincentLajoie <125910689+THzWeeds@users.noreply.github.com>
2024-09-11 09:33:50 -0400

    Revert "Merge pull request #11 from cogi234/Vincent"
    This reverts commit e279dfdcd23e394d993ab422ad6f3a1f157b811a, reversing
    changes made to b27691cb785575ddbfaba06993633696d9d67376.

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,15 +1,15 @@
 <?php
 
 namespace App\Models;
- 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
@@ -113,5 +113,4 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->belongsToMany(Tag::class, "blocked_tags");
     }
-
 }
--- resources/views/livewire/profile/update-profile-information-form.blade.php
+++ resources/views/livewire/profile/update-profile-information-form.blade.php
@@ -65,46 +65,39 @@ new class extends Component
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Informations de profil') }}
+            {{ __('Profile Information') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __("Mettez à jour les informations de profil et l'adresse e-mail de votre compte.") }}
+            {{ __("Update your account's profile information and email address.") }}
         </p>
     </header>
 
     <form wire:submit="updateProfileInformation" class="mt-6 space-y-6">
         <div>
-            <x-input-label for="name" :value="__('Nom')" />
+            <x-input-label for="name" :value="__('Name')" />
             <x-text-input wire:model="name" id="name" name="name" type="text" class="mt-1 block w-full" required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
 
         <div>
-            <x-input-label for="email" :value="__('Courriel')" />
+            <x-input-label for="email" :value="__('Email')" />
             <x-text-input wire:model="email" id="email" name="email" type="email" class="mt-1 block w-full" required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
-                @if ( auth()->user()->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2  text-green-600 dark:text-green-400">
-                        {{ __('Votre adresse email est vérifiée.') }}
-                    </p>
 
-                </div>
-            @endif
             @if (auth()->user() instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! auth()->user()->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
-                        {{ __('Votre adresse email n\'est pas vérifiée.') }}
+                        {{ __('Your email address is unverified.') }}
 
                         <button wire:click.prevent="sendVerification" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                            {{ __('Cliquez ici pour renvoyer le courriel de vérification.') }}
+                            {{ __('Click here to re-send the verification email.') }}
                         </button>
                     </p>
 
                     @if (session('status') === 'verification-link-sent')
                         <p class="mt-2 font-medium text-sm text-green-600 dark:text-green-400">
-                            {{ __('Un nouveau lien de vérification a été envoyé à votre adresse courriel') }}
+                            {{ __('A new verification link has been sent to your email address.') }}
                         </p>
                     @endif
                 </div>
@@ -112,10 +105,10 @@ new class extends Component
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Enregistrée') }}</x-primary-button>
+            <x-primary-button>{{ __('Save') }}</x-primary-button>
 
             <x-action-message class="me-3" on="profile-updated">
-                {{ __('Enregistrée') }}
+                {{ __('Saved.') }}
             </x-action-message>
         </div>
     </form>
