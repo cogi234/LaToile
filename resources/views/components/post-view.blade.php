e78ed72f2a13bbcb21b4314f02dcbefe18ab5067
cogi234 <colinbougie@gmail.com>
2024-09-11 08:41:22 -0400

    Update post-view.blade.php

--- resources/views/components/post-view.blade.php
+++ resources/views/components/post-view.blade.php
@@ -69,10 +69,3 @@
         </div>
     </div>
 </div>
-
-<div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg text-gray-900 dark:text-gray-100">
-    <div class="text-lg font-bold">
-        {{ $post->user->name }}
-    </div>
-    <hr />
-</div>
