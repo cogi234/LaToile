6e482c279ab0e7151164680b58473c1d017abc2d
NAGZY <68570686+NAGZYY@users.noreply.github.com>
2024-09-12 13:46:49 -0400

    2024-09-12

--- resources/views/components/post-view.blade.php
+++ resources/views/components/post-view.blade.php
@@ -28,7 +28,7 @@
     <div class="flex justify-between items-center">
         <div class="post-actions mt-4 flex items-center">
             <!-- J'aime -->
-            <button title="Aimer" class="like-button flex items-center text-gray-600 dark:text-gray-400 dark:hover:text-red-500 mr-4">
+            <button title="Aimer" class="like-button flex items-center text-gray-600 dark:text-gray-400 hover:text-red-500 mr-4 dark:hover:text-red-500 mr-4">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                 </svg>
