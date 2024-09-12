ac9728e6351a94939926797e7a3cdf898fd2392c
Colin Bougie <colinbougie@gmail.com>
2024-09-12 12:11:56 -0400

    Follow button (#17)
    * started form and function
    
    * Simplifier
    
    On veux juste creer des posts de texts.
    Pas besoin de gosser pour pouvoir en modifier ou gerer des images pour l'instant
    
    * css
    
    * Posts are JSON arrays. This is now part of the model
    
    We can now access the contents of a post as an array.
    
    * colin account et pas de form si pas connecter
    
    * Simple post component
    
    * Integration affichage post
    
    * Update post-view.blade.php
    
    * Follow button now functional
    
    * post user
    
    unused for now

--- resources/views/components/post-user.blade.php
+++ resources/views/components/post-user.blade.php
@@ -1,10 +1,17 @@
+
+
 <div class="post-header flex items-center">
     <!-- Image de profil -->
     <img src="{{ $user->avatar ?? 'path/to/default/avatar.png' }}" alt="Profile Image" class="w-12 h-12 rounded-full mr-4 shadow-lg">
 
     <!-- Nom et date -->
     <div>
-        <h4 class="text-lg font-bold text-gray-900 dark:text-gray-100">{{ $user->name }}</h4>
-        <p class="text-sm text-gray-600 dark:text-gray-400">{{ strftime('%d %B %Y à %H:%M', strtotime($post->created_at)) }}</p>
+        <h4 class="text-lg font-bold text-gray-900 dark:text-gray-100">
+            {{ $user->name }}
+            @auth
+                <livewire:user.follow id="{{ $post->user->id }}" />
+            @endauth
+        </h4>
     </div>
+    <hr/>
 </div>
