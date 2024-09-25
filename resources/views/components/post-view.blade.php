703bbb466a735c8bcae4fc67b340852650cadc10
NAGZY <68570686+NAGZYY@users.noreply.github.com>
2024-09-25 09:44:21 -0400

    try

--- resources/views/components/post-view.blade.php
+++ resources/views/components/post-view.blade.php
@@ -1,4 +1,4 @@
-<div id="post-{{ $post->id }}" onclick="window.open('/post/{{ $post->id }}', '_blank')" {{ $attributes->merge(['class' => "cursor-pointer post bg-white hover:bg-white/50 dark:bg-gray-800 dark:hover:dark:bg-gray-700 overflow-hidden shadow-sm rounded-lg mb-4 md:p-5 p-2 md:mb-5 mb-3 w-full"]) }}>
+<div id="post-{{ $post->id }}" onclick="window.location.href = '/post/{{ $post->id }}'" {{ $attributes->merge(['class' => "cursor-pointer post bg-white hover:bg-white/50 dark:bg-gray-800 dark:hover:dark:bg-gray-700 overflow-hidden shadow-sm rounded-lg mb-4 md:p-5 p-2 md:mb-5 mb-3 w-full"]) }}>
     <!-- L'utilisateur qui a publier le post -->
     <x-post-user
         :user="$post->user"
@@ -80,8 +80,10 @@
     </div>
 </div>
 
-<div id="editPostModal" class="modal hidden fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex items-center justify-center">
-    <div class="modal-content top-1/4 w-2/4 p-4 pt-2 mx-auto bg-white dark:bg-gray-800 overflow-hidden shadow-sm rounded-lg">
+<div id="editPostModal"
+    class="modal hidden fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex items-center justify-center">
+    <div
+        class="modal-content w-full md:w-6/12 top-1/4 w-2/4 p-4 pt-2 mx-auto bg-white dark:bg-gray-800 overflow-hidden shadow-sm rounded-lg">
         <div class="flex flex-row-reverse pb-2">
             <!-- Close button -->
             <button onclick="closeEditPopup()" title="Fermez le panneau"
@@ -103,8 +105,47 @@
             <textarea name="newContent" id="postContent" placeholder="Texte du post modifié ici" rows="5" class="w-full p-2 block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
                     rounded-md shadow-sm bg-white dark:bg-gray-800 text-black dark:text-white min-h-20 rounded" minlength="5" required>{{ $textContent }}</textarea>
             <div class="flex justify-end mt-4">
-                <button type="button" onclick="closeEditPopup()" class="mr-2 px-4 py-2 bg-gray-300 dark:bg-gray-100/50 hover:bg-gray-400 rounded transition ease-in-out duration-150">Annuler</button>
-                <button type="submit" class="px-4 py-2 bg-gray-800 hover:bg-gray-700 dark:hover:bg-white dark:bg-gray-200 text-gray-100 rounded text-white dark:text-black transition ease-in-out duration-150">Modifier le post</button>
+                <button type="button" onclick="closeEditPopup()"
+                    class="mr-2 px-4 py-2 bg-gray-300 dark:bg-gray-100/50 hover:bg-gray-400 rounded transition ease-in-out duration-150">Annuler</button>
+                <button type="submit"
+                    class="px-4 py-2 bg-gray-800 hover:bg-gray-700 dark:hover:bg-white dark:bg-gray-200 text-gray-100 rounded text-white dark:text-black transition ease-in-out duration-150">Modifier
+                    le post</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+<div id="deleteConfirmationModal"
+    class="modal hidden fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex items-center justify-center">
+    <div
+        class="modal-content w-full md:w-6/12 top-1/4 w-2/4 p-4 pt-2 mx-auto bg-white dark:bg-gray-800 overflow-hidden shadow-sm rounded-lg">
+        <div class="flex flex-row-reverse pb-2">
+            <!-- Close button -->
+            <button onclick="closeDeleteConfirmationPopup()" title="Fermez le panneau"
+                class="ml-2 flex items-center text-gray-600 dark:text-gray-400 hover:text-red-400 dark:hover:text-red-500">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
+                    stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+            </button>
+        </div>
+        @php
+        $contentArray = is_array($post->content) ? $post->content : json_decode($post->content, true);
+        $textContent = isset($contentArray[0]['content']) ? $contentArray[0]['content'] : '';
+        @endphp
+        <form action="{{ route('posts.deletePost', $post->id) }}" method="POST" style="display: inline;">
+            @csrf
+            @method('DELETE')
+            <span class="text-xl flex flex-row pb-2 text-black dark:text-white">Êtes-vous sûr de vouloir supprimer ce post?</span>
+            <span class="text-l flex flex-row pb-2 text-black dark:text-white">Celui-ci sera supprimé définitivement.</span>
+
+            <div class="flex justify-end mt-4">
+                <button type="button" onclick="closeDeleteConfirmationPopup()"
+                    class="mr-2 px-4 py-2 bg-gray-300 dark:bg-gray-100/50 hover:bg-gray-400 rounded transition ease-in-out duration-150">Annuler</button>
+                <button type="submit"
+                    class="px-4 py-2 bg-gray-800 hover:bg-red-600 dark:hover:bg-red-800 dark:bg-gray-200 text-gray-100 rounded text-white dark:text-black transition ease-in-out duration-150">
+                    Supprimer définitivement
+                </button>
             </div>
         </form>
     </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,8 +11,10 @@
                     active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500
                     focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
                 onclick="showPostEditor()">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mr-3 inline-block">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-6 mr-3 inline-block">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                 </svg>
 
                 Publier un post
@@ -21,7 +23,7 @@
             @endauth
 
             <!-- Ajout des onglets -->
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm rounded-lg mb-3 md:mb-5">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm rounded-lg mb-3 md:mb-5 mt-14 xl:mt-0">
                 <div class="tabs p-6 text-gray-900 dark:text-gray-100">
                     <!-- Les blocs sont maintenant des liens entièrement cliquables -->
                     <a href="javascript:void(0);" class="tab active" id="all-tab" onclick="showContent('all')">
@@ -105,17 +107,8 @@
     </style>
 
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            // Charger l'onglet sélectionné précédemment
-            const lastTab = localStorage.getItem('lastTab') || 'all'; // 'all' par défaut
-            showContent(lastTab);
-        });
-
         function showContent(tab) {
-            // Enregistrer l'onglet actif dans localStorage
-            localStorage.setItem('lastTab', tab);
-
-            // Envoyer l'event pour reset le contenu des tabs
+            //Envoyer l'event pour reset le contenu des tabs
             this.dispatchEvent(
                 new Event('reset-post-views')
             );
@@ -138,16 +131,6 @@
             document.getElementById(tab + '-content').style.display = 'block';
             document.getElementById(tab + '-tab').classList.add('active');
         }
-
-        function openEditPopup() {
-            document.getElementById('editPostModal').classList.remove('hidden');
-        }
-
-        // Fonction pour fermer le modal
-        function closeEditPopup() {
-            document.getElementById('editPostModal').classList.add('hidden');
-        }
     </script>
-
 
 </x-app-layout>
