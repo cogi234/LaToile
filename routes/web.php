db0d7345be77f68ba9cca45eb196be5ed6143e5b
cogi234 <colinbougie@gmail.com>
2024-10-16 21:16:19 -0400

    Preferences page

--- routes/web.php
+++ routes/web.php
@@ -44,13 +44,11 @@ Route::get('/email/verify', function () {
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
-
     return redirect('/home');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
-
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
