ed00b13fed90f52ce859f43419693ba52a13e637
NAGZY <68570686+NAGZYY@users.noreply.github.com>
2024-10-23 09:33:57 -0400

    Revert "Fix bug"
    This reverts commit e4c2796b08b2a8d3cd0d4a6134b60777d0a3d6ee.

--- resources/views/livewire/admin/false-report.blade.php
+++ resources/views/livewire/admin/false-report.blade.php
@@ -23,7 +23,7 @@ new class extends Component {
 };
 ?>
 
-<button wire:click="setFalseReport({{$reportId}})" title="Marqué le report comme traité"
+<button wire:click="setFalseReport({{$reportId}})" title="Marqué le report comme traité car il s'agît d'un faux report"
 class="repost-button flex items-center text-gray-600 dark:text-gray-400 hover:text-green-500 dark:hover:blue-green-500 mr-4"
 onclick="event.stopPropagation()">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -31,5 +31,5 @@ onclick="event.stopPropagation()">
         <path stroke-linecap="round" stroke-linejoin="round"
             d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" />
     </svg>
-    <span class="ml-1">Report traité</span>
+    <span class="ml-1">Faux report</span>
 </button>
