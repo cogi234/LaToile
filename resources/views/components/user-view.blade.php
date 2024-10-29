1ca932a5e7cb3b058cbe400178b9a9e42a61a74b
cogi234 <colinbougie@gmail.com>
2024-10-29 19:40:43 -0400

    ban indicator on user page

--- resources/views/components/user-view.blade.php
+++ resources/views/components/user-view.blade.php
@@ -83,6 +83,18 @@
                 </div>
             </div>
             @endif
+            @if($user->isBanned())
+            <div class="flex flex-row items-center">
+                <span class="text-red-500 font-bold mr-1">Banni</span>
+                <div title="Banni">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" 
+                    stroke="currentColor" class="size-4 text-red-500 mr-1">
+                        <path stroke-linecap="round" stroke-linejoin="round" 
+                            d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636" />
+                    </svg>
+                </div>
+            </div>
+            @endif
         </div>
     </div>
 </div>
