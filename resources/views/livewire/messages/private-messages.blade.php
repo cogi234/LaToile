9f360f710e995f1b171791de15994d6c5aaaa723
MathieuLeroux1 <116597068+MathieuLeroux1@users.noreply.github.com>
2024-11-11 14:30:56 -0500

    Update private-messages.blade.php

--- resources/views/livewire/messages/private-messages.blade.php
+++ resources/views/livewire/messages/private-messages.blade.php
@@ -318,7 +318,7 @@ new class extends Component {
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                             </svg>                              
                         </button>
-                        <div title="{{ $message->updated_at->setTimezone($currentTimeZone)->format($timeFormat) }}"
+                        <div title="{{ $message->created_at->setTimezone($currentTimeZone)->format($timeFormat) }}"
                             id="message_{{ $message->id }}" wire:click.stop
                             class="lg:max-w-[60%] max-w-[90%] w-auto p-3 rounded-lg bg-blue-500 text-white"
                             >
