b3651f5ae8a85e3e9467ee248eb46d4e9ab0e80b
MathieuLeroux1 <116597068+MathieuLeroux1@users.noreply.github.com>
2024-11-11 15:55:34 -0500

    Update private-messages.blade.php

--- resources/views/livewire/messages/private-messages.blade.php
+++ resources/views/livewire/messages/private-messages.blade.php
@@ -210,20 +210,36 @@ new class extends Component {
             </div>
             <!-- Search Bar -->
             <div class="p-4" >
-                <div class="relative">
-                    <input type="text" name="query" id="searchBar"
-                        class="block w-full pl-10 pr-4 py-2 bg-gray-200 dark:bg-slate-600 text-gray-900 dark:text-white rounded-full focus:outline-none focus:bg-white focus:text-gray-900 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-sm"
-                        placeholder="Rechercher des Messages Directs"/>
-            
-                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <div class="flex">
+                    <div class="inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                         <svg class="w-5 h-5 text-gray-500 dark:text-gray-300" fill="none" stroke="currentColor"
                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35m2.1-6.95a7.5 7.5 0 1 1-15 0 7.5 7.5 0 0 1 15 0z"></path>
                         </svg>
                     </div>
+
+                    <input type="text" name="query" id="searchBar"
+                        class="block w-full pl-10 pr-4 py-2 bg-gray-200 dark:bg-slate-600 text-gray-900 dark:text-white rounded-full focus:outline-none focus:bg-white focus:text-gray-900 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-sm"
+                        placeholder="Rechercher des Messages Directs"/>
                 </div>
             </div>
-
+            <div class="p-4">
+                <!-- Conteneur avec le contour et les styles -->
+                <div class="flex items-center bg-gray-200 dark:bg-slate-600 text-gray-700 dark:text-gray-300 rounded-full pl-3 pr-4 py-2 focus:outline-none focus:bg-white focus:text-gray-900 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                    <!-- Icône de recherche -->
+                    <div class="pointer-events-none flex items-center">
+                        <svg class="w-5 h-5 text-gray-500 dark:text-gray-300" fill="none" stroke="currentColor"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35m2.1-6.95a7.5 7.5 0 1 1-15 0 7.5 7.5 0 0 1 15 0z"></path>
+                        </svg>
+                    </div>
+                    <!-- Champ de recherche -->
+                    <input type="text" name="query" id="searchBar"
+                        class="block w-full pl-2 bg-transparent focus:outline-none text-gray-700 dark:text-gray-300 rounded-full"
+                        placeholder="Rechercher des Messages Directs"/>
+                </div>
+            </div>
+            
             <!-- List of Conversations -->
             <div>
                 @if($targetUserId !== null && !in_array($targetUserId, $uniqueSenderIds))
