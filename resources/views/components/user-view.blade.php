64af3f3d4e6718186f6c442f80188458087e543e
MathieuLeroux1 <116597068+MathieuLeroux1@users.noreply.github.com>
2024-11-20 17:34:43 -0500

    Update user-view.blade.php

--- resources/views/components/user-view.blade.php
+++ resources/views/components/user-view.blade.php
@@ -7,7 +7,7 @@
 
                 {{-- Nom et Abonnés / Abonnement --}}
                 <div class="mr-2">
-                    <div class="flex flex-row">
+                    <div class="flex flex-row gap-3">
                         <h2 class="text-xl font-semibold text-black dark:text-gray-100 mr-2">{{ $user->name }}</h2>
                         
                         @auth
@@ -15,20 +15,7 @@
                         <livewire:user.follow id="{{ $user->id }}" />
                         @endif
                         @endauth
-                    </div>
-                    <div class="flex mb-4">
-                        @if($user->moderator)
-                            <div class="flex flex-row items-center">
-                                <p class="text-green-500 font-bold">Modérateur</p>
-                                <div title="Modérateur vérifié">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                        stroke="currentColor" class="size-4 text-green-500 ml-1">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
-                                    </svg>
-                                </div>
-                            </div>
-                        @endif
+
                         @if($user->isBanned())
                             <div class="flex flex-row items-center">
                                 <span class="text-red-500 font-bold mr-1">Banni</span>
@@ -43,6 +30,20 @@
                         @endif
                         @if (Auth::Check())
                             <livewire:user.blocked-user-check id="{{ $user->id }}" />
+                        @endif
+                    </div>
+                    <div class="flex mb-4">
+                        @if($user->moderator)
+                            <div class="flex flex-row items-center">
+                                <p class="text-green-500 font-bold">Modérateur</p>
+                                <div title="Modérateur vérifié">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                        stroke="currentColor" class="size-4 text-green-500 ml-1">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+                                    </svg>
+                                </div>
+                            </div>
                         @endif
                     </div>
                     <div class="flex space-x-4 mt-2">
