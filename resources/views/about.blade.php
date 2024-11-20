2af14336ae85ff2f973fe71a1dbc72991da62971
cogi234 <colinbougie@gmail.com>
2024-11-19 22:15:17 -0500

    Started about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,29 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-gray-900 dark:text-gray-100">
-            <div class="relative text-4xl text-center mb-20">À propos de ce site</div>
-            <div class="relative mt-20">
-                Ce site web à été créé par Colin Bougie, Jérémy Racine, Mathieu Leroux et Vincent Lajoie en automne 2024.
+            <div class="text-4xl text-center mb-10">
+                À propos de ce site
+            </div>
+            <div class="mb-5">
+                Ce site web à été créé par Colin Bougie, Jérémy Racine, Mathieu Leroux et Vincent Lajoie en automne 2024 comme projet d'école.
+            </div>
+            <div class="mb-5">
+                Ce site est un réseau social, incluant la plupart des fonctionnalités attendues d'un réseau social, comme:
+                <ul class="list-disc list-inside indent-8">
+                    <li>Des posts pouvant contenir du texte, des images, des vidéos et de l'audio.</li>
+                    <li>Des mentions J'aime sur les posts</li>
+                    <li>La possibilité de partager ou répondre à un post</li>
+                    <li>La possibilité de suivre un utilisateur</li>
+                    <li>Un système de tags</li>
+                    <li>La possibilité de sauvegarder des brouillons</li>
+                    <li>La possibilité de planifier la publication d'un post pour un temps spécifique</li>
+                    <li>Un système de messagerie, incluant des groupes</li>
+                    <li>Des notifications</li>
+                    <li>La possibilité de bloquer un autre utilisateur pour ne pas interagir avec</li>
+                    <li>La possibilité de bloquer un tag pour ne pas voir les posts associés</li>
+                    <li>La possibilité de reporter un post ou un message pour que des modérateurs lui portent attention</li>
+                    <li>Etc.</li>
+                </ul>
             </div>
         </div>
     </div>
