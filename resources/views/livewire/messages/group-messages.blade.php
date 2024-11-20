8d0bf9e8e9aefe1a8fda95096ef56786583fbfdd
NAGZY <68570686+NAGZYY@users.noreply.github.com>
2024-11-20 09:00:09 -0500

    Fix report in group message

--- resources/views/livewire/messages/group-messages.blade.php
+++ resources/views/livewire/messages/group-messages.blade.php
@@ -444,15 +444,16 @@ new class extends Component {
                                         <div title="{{ $message->created_at->setTimezone($currentTimeZone)->format($timeFormat) }}"
                                             class="flex flex-nowrap justify-center items-center p-3 rounded-lg bg-gray-300 text-gray-900">
                                             <div class="hidden group-hover:block pl-4">
-                                                <button title="Signaler le message"
-                                                    class="share-button flex lg:mb-0 mb-2 items-center text-gray-900 dark:text-gray-900 hover:text-orange-400 dark:hover:text-orange-400 mr-2"
-                                                    onclick="event.stopPropagation(); showReportMessageModal({{$message->id}}, 'PrivateMessage');">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                                        stroke="currentColor" class="size-6">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+                                            <!-- Signaler -->
+                                            <button title="Signaler le message"
+                                                class="share-button flex lg:mb-0 mb-2 items-center text-gray-900 dark:text-gray-900 hover:text-orange-400 dark:hover:text-orange-400 mr-2"
+                                                onclick="event.stopPropagation(); showReportMessageModal({{$message->id}}, 'GroupMessage');">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                    stroke="currentColor" class="size-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
                                                     </svg>
-                                                </button>
+                                            </button>
                                             </div>
                                             <div class="break-words w-full pr-4">
                                                 <p class="break-words">{!! $messageText !!}</p>
