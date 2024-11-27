2c32da5e06f7576f050a8e910a71c8b46dfab9f3
cogi234 <colinbougie@gmail.com>
2024-11-26 20:22:34 -0500

    fix route name conflict

--- routes/web.php
+++ routes/web.php
@@ -97,16 +97,16 @@ Route::view('about', 'about')
 //Emails
 Route::get('/email/verify', function () {
     return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
+})->middleware('auth');
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
     return redirect('/home');
-})->middleware(['auth', 'signed'])->name('verification.verify');
+})->middleware(['auth', 'signed']);
 
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
     return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+})->middleware(['auth', 'throttle:6,1']);
 
 require __DIR__ . '/auth.php';
